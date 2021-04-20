04f10828a203c5790f29220bcf9e5ebbdf2e790b
Gilbert Komin <gilbert.komin@monterail.com>
2021-04-20 15:41:24 +0200

    Set minimize false in build config

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -12,7 +12,7 @@ function createConfig(entry, output) {
     entry,
     output,
     optimization: {
-      minimizer: [new UglifyJSPlugin()],
+      minimize: false,
     },
     module: {
       rules: [
